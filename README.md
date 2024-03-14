e67f7a248b7f67ee205d46d1487ba15af6db99c5
liuzhigang06 <liuzhigang06@kuaishou.com>
2024-03-14 17:19:47 +0800

    change app.jsx

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 ## Usage
 - 安装依赖 `npm install`
 - 启动项目 `npm run dev`
+- 打开页面 `http://localhost:8100/`
+
+## sdk配置
+- 具体查看 `src/App.tsx`内的 `<PcSpaDesigner />`组件的配置
--- src/App.tsx
+++ src/App.tsx
@@ -13,151 +13,6 @@ const getQuery = () => {
   return location.search.replace(/\?/, '')
 }
 
-// const Designer = window.mybricks.SPADesigner
-
-/**
- * 配置设计器
- */
-// const config = ({ save }) => {
-//   return {
-//     shortcuts: {
-//       'ctrl+s': [save]
-//     },
-//     plugins: [
-//       servicePlugin(),
-//       tools(),
-//     ],
-//     comLibLoader(desc) {
-//       //加载组件库
-//       return new Promise((resolve, reject) => {
-
-//         resolve([
-//           // 'http://localhost:8002/libEdt.js',
-//           // 'http://localhost:8001/libEdt.js',
-//           // 'https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/7182_1.0.92/2023-11-30_11-43-16/edit.js',
-//           'https://assets.mybricks.world/comlibs/mybricks.normal-pc/1.4.46/2023-12-12_11-53-35/edit.js'
-//         ])
-//       })
-//     },
-//     pageContentLoader() {
-//       //加载页面内容
-//       // const pageContent = window.localStorage.getItem('--mybricks--')
-
-//       return new Promise((resolve, reject) => {
-//         const searchParam = getQuery()
-//         let pageContent = window.localStorage.getItem(
-//           `--mybricks--${searchParam ? searchParam : ''}`
-//         )
-//         if (pageContent) {
-//           pageContent = JSON.parse(pageContent)
-
-//           resolve(pageContent)
-//         } else {
-//           resolve({})
-//         }
-//       })
-//     },
-//     geoView: {//配置布局视图
-//       nav: {float: false},
-//       scenes: {
-//         adder: [
-//           {
-//             type: 'popup',
-//             title: '对话框',
-//             template: {
-//               namespace: 'mybricks.basic-comlib.popup',
-//               deletable: false,
-//               asRoot: true
-//             }
-//           }
-//         ]
-//       },
-//     },
-//     toplView: {
-//       title: '交互',
-//       cards: {
-//         main: {
-//           title: '页面',
-//         },
-//       },
-//       globalIO: {
-//         startWithSingleton: true,
-//       },
-//       vars: {},
-//       fx: {},
-//       useStrict: false,
-//     },
-//     com: {
-//       //组件运行配置
-//       env: {
-//         i18n(title) {
-//           //多语言
-//           return title
-//         },
-//         callConnector(connector, params) {
-//           //调用连接器
-//           if (connector.type === 'http') {
-//             //服务接口类型
-//             return callConnectorHttp(connector, params, {
-//               // 发送请求前的钩子函数
-//               before(options) {
-//                 return {
-//                   ...options,
-//                 }
-//               },
-//             })
-//           } else {
-//             return Promise.reject('错误的连接器类型.')
-//           }
-//         },
-//         get getRouter() {
-//           const toast = (info: string) => {
-//             message.info(info);
-//           };
-//           return () => ({
-//             reload: () => toast('reload'),
-//             redirect: ({ url }: { url: string }) => toast(`redirect: ${url}`),
-//             back: () => toast('back'),
-//             forward: () => toast('forward'),
-//             pushState: ({
-//               state,
-//               title,
-//               url,
-//             }: {
-//               state: any;
-//               title: string;
-//               url: string;
-//             }) =>
-//               toast(`pushState: ${JSON.stringify({ state, title, url })}`),
-//             openTab: ({ url, title }: { url: string; title: string }) =>
-//               toast(`open a new tab: ${JSON.stringify({ url, title })}`),
-//           });
-//         },
-//       },
-//       events: [
-//         //配置事件
-//         {
-//           type: 'jump',
-//           title: '跳转到',
-//           exe({ options }) {
-//             const page = options.page
-//             if (page) {
-//               window.location.href = page
-//             }
-//           },
-//           options: [
-//             {
-//               id: 'page',
-//               title: '页面',
-//               editor: 'textarea',
-//             },
-//           ],
-//         },
-//       ],
-//     },
-//   }
-// }
-
 export default function MyDesigner() {
   const designerRef = useRef<{ switchActivity; dump; toJSON }>()
   const [pageContent, setPageContent] = useState({
@@ -177,7 +32,7 @@ export default function MyDesigner() {
     ],
     debugMockConfig: {
       debugQuery: {},
-      debugMainProps: {},
+      debugProps: {},
       localStorageMock: [],
       debugHeaders: [],
       sessionStorageMock: [],
@@ -319,7 +174,9 @@ export default function MyDesigner() {
                   return items
                 }}
                 plugins={((plugins) => {
-                  return plugins
+                  const index = plugins.findIndex(item => item.name === 'locale')
+                  const newPlugins = plugins.splice(index, 1)
+                  return newPlugins
                 })}
                 envExtra={{ // 扩展额外的 env
                   test: () => {
